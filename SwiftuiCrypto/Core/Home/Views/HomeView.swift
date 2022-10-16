79d2ce62c0a3796b96a393176accbb0abb75b524
sandalwoods <1764076874@qq.com>
2022-10-16 22:13:57 +0800

    video 3

--- SwiftuiCrypto/Core/Home/Views/HomeView.swift
+++ SwiftuiCrypto/Core/Home/Views/HomeView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // background lyaer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                Spacer(minLength: 0)
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+        
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio))
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
--- SwiftuiCrypto/SwiftuiCryptoApp.swift
+++ SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftuiCryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
