ce280e8e3928f0e6586ed133d6b6d23e35bdce5c
sandalwoods <1764076874@qq.com>
2022-10-16 18:17:37 +0800

    Initial Commit

---
+++ SwiftuiCrypto/SwiftuiCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCryptoApp.swift
+//  SwiftuiCrypto
+//
+//  Created by kevin on 2022/10/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
